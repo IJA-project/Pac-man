95353de04ac982b05207ae8202c0e733aed8d218
Kateryna Zdebska <zdebska.kateryna@gmail.com>
2023-05-06 23:43:00 +0200

    maze selection made

--- src/MazeSelection.java
+++ src/MazeSelection.java
@@ -1,14 +1,23 @@
 package src;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 
 public class MazeSelection {
     public MazeSelection(){
@@ -19,23 +28,38 @@ public class MazeSelection {
             frame.setResizable(false);
             frame.setBackground(Color.BLACK);
             
-            JPanel buttonPanel = new JPanel();
-            buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.Y_AXIS));
+            JPanel mainPanel = new JPanel(new BorderLayout());
+            mainPanel.setBackground(Color.BLACK);
+            mainPanel.setPreferredSize(new Dimension(600, 600));
+            JPanel buttonPanel = new JPanel(new GridLayout(2, 2, 10, 10));
+            buttonPanel.setBorder(BorderFactory.createEmptyBorder(20, 310, 20, 310));
+            // JPanel buttonPanel = new JPanel();
+            // buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.Y_AXIS));
+            buttonPanel.setPreferredSize(new Dimension(600, 600));
             buttonPanel.setBackground(Color.BLACK);
-            JButton playButton = new JButton("Maze 1");
-            playButton.setAlignmentX(Component.CENTER_ALIGNMENT);
-            playButton.addActionListener(new ActionListener() {
+
+            ImageIcon maze1_icon = new ImageIcon("data\\img\\maze1.png");
+            Image scaledImage1 = maze1_icon.getImage().getScaledInstance(280, 280, Image.SCALE_SMOOTH);
+            ImageIcon scaledIcon1 = new ImageIcon(scaledImage1);
+            JButton maze1Button = new JButton();
+            maze1Button.setIcon(scaledIcon1);
+            // playButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+            maze1Button.addActionListener(new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent arg0){
                     frame.setVisible(false);
                     PacmanGame pacmanGame = new PacmanGame(1, 0, "data\\maps\\map1.txt");
                 }
             });
-            buttonPanel.add(playButton);
+            buttonPanel.add(maze1Button);
             
-            JButton savedGameButton = new JButton("Maze 2");
-            savedGameButton.setAlignmentX(Component.CENTER_ALIGNMENT);
-            savedGameButton.addActionListener(new ActionListener() {
+            ImageIcon maze2_icon = new ImageIcon("data\\img\\maze2.png");
+            Image scaledImage2 = maze2_icon.getImage().getScaledInstance(280, 280, Image.SCALE_SMOOTH);
+            ImageIcon scaledIcon2 = new ImageIcon(scaledImage2);
+            JButton maze2Button = new JButton();
+            maze2Button.setIcon(scaledIcon2);
+            // savedGameButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+            maze2Button.addActionListener(new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent arg0){
                     // Code to view high scores goes here
@@ -43,22 +67,52 @@ public class MazeSelection {
                     PacmanGame pacmanGame = new PacmanGame(1, 0, "data\\maps\\map2.txt");
                 }
             });
-            buttonPanel.add(savedGameButton);
+            buttonPanel.add(maze2Button);
             
-            // JButton exitButton = new JButton("Maze 3");
+            ImageIcon maze3_icon = new ImageIcon("data\\img\\maze3.png");
+            Image scaledImage3 = maze3_icon.getImage().getScaledInstance(280, 280, Image.SCALE_SMOOTH);
+            ImageIcon scaledIcon3 = new ImageIcon(scaledImage3);
+            JButton maze3Button = new JButton();
+            maze3Button.setIcon(scaledIcon3);
             // exitButton.setAlignmentX(Component.CENTER_ALIGNMENT);
-            // exitButton.addActionListener(new ActionListener() {
-            //     @Override
-            //     public void actionPerformed(ActionEvent arg0){
-            //         // Code to show options goes here
-            //         frame.setVisible(false);
-            //         PacmanGame pacmanGame = new PacmanGame(1, 0);
+            maze3Button.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent arg0){
+                    // Code to show options goes here
+                    frame.setVisible(false);
+                    PacmanGame pacmanGame = new PacmanGame(1, 0, "data\\maps\\map3.txt");
                     
-            //     }
-            // });
-            // buttonPanel.add(exitButton);
-            
-            frame.add(buttonPanel);
+                }
+            });
+            buttonPanel.add(maze3Button);
+
+            ImageIcon maze4_icon = new ImageIcon("data\\img\\maze4.png");
+            Image scaledImage4 = maze4_icon.getImage().getScaledInstance(280, 280, Image.SCALE_SMOOTH);
+            ImageIcon scaledIcon4 = new ImageIcon(scaledImage4);
+            JButton maze4Button = new JButton();
+            maze4Button.setIcon(scaledIcon4);
+            // exitButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+            maze4Button.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent arg0){
+                    // Code to show options goes here
+                    frame.setVisible(false);
+                    PacmanGame pacmanGame = new PacmanGame(1, 0, "data\\maps\\map4.txt");
+                    
+                }
+            });
+            buttonPanel.add(maze4Button);
+
+            JLabel textLabel = new JLabel("Chose your maze");
+            textLabel.setPreferredSize(new Dimension(300, 80));
+            textLabel.setBackground(Color.BLACK);
+            textLabel.setHorizontalAlignment(SwingConstants.CENTER);
+            textLabel.setVerticalAlignment(SwingConstants.CENTER);
+            textLabel.setForeground(Color.yellow);
+            textLabel.setFont(new Font("Bauhaus 93", Font.BOLD, 40));
+            mainPanel.add(textLabel, BorderLayout.NORTH);
+            mainPanel.add(buttonPanel, BorderLayout.CENTER);
+            frame.add(mainPanel, BorderLayout.CENTER);
             frame.setVisible(true);
     }
     
